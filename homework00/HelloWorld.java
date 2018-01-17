92b73cb3b2028ec5d4e3b7d3705c102d4666bb54
elopez47 <35274682+elopez47@users.noreply.github.com>
2018-01-16 19:52:07 -0800

    Create HelloWorld.java

---
+++ homework00/HelloWorld.java
@@ -0,0 +1,11 @@
+/*
+ * Eva Lopez
+ * Homework 00
+ * CMSI 186
+ */
+
+public class HelloWorld {
+  public static void main( String[] args ) {
+    System.out.println( " Hello, \n world! " );
+  }
+}
