97944c2ce5f8b634421e9765b0db6f7702af7f07
elopez47 <35274682+elopez47@users.noreply.github.com>
2018-02-13 12:03:07 -0800

    Created Die.java

---
+++ homework03/Die.java
@@ -0,0 +1,96 @@
+/*
+
+ Name: Eva Lopez
+ File name: Die.java
+ Purpose: Determines if a given year is a leap year, the days in a given month,
+   returns whether or not a date is valid, and counts the number of days between
+   two dates.
+ Date:  2018-02-17
+
+*/
+public class Die {
+
+  /**
+   * private instance data
+   */
+   private int sides;
+   private int pips;
+   private final int MINIMUM_SIDES = 4;
+   public int nSides;
+
+   // public constructor:
+  /**
+   * constructor
+   * @param nSides int value containing the number of sides to build on THIS Die
+   * @throws       IllegalArgumentException
+   * Note: parameter must be checked for validity; invalid value must throw "IllegalArgumentException"
+   */
+   public Die( int nSides ) {
+      this.nSides = nSides;
+      if ( nSides >= MINIMUM_SIDES ) {
+      } else {
+         throw new IllegalArgumentException( "Minimum number of sides is 4." );
+      }
+   }
+
+  /**
+   * Roll THIS die and return the result
+   * @return  integer value of the result of the roll, randomly selected
+   */
+   public int roll() {
+      return (int)(Math.floor(Math.random() * this.nSides) + 1);
+   }
+
+  /**
+   * Get the value of THIS die to return to the caller; note that the way
+   *  the count is determined is left as a design decision to the programmer
+   *  For example, what about a four-sided die - which face is considered its
+   *  "value"?
+   * @return the pip count of THIS die instance
+   */
+   public int getValue() {
+      return this.roll();
+   }
+
+  /**
+   * @param  int  the number of sides to set/reset for this Die instance
+   * @return      The new number of sides, in case anyone is looking
+   * @throws      IllegalArgumentException
+   */
+   public void setSides( int sides ) {
+     if ( sides >= MINIMUM_SIDES ) {
+       this.nSides = sides;
+     } else {
+        throw new IllegalArgumentException( "Minimum number of sides is 4." );
+     }
+   }
+
+  /**
+   * Public Instance method that returns a String representation of THIS die instance
+   * @return String representation of this Die
+   */
+   public String toString() {
+      String result = "[" + this.getValue() + "]";
+      return result;
+   }
+
+  /**
+   * Class-wide method that returns a String representation of THIS die instance
+   * @return String representation of this Die
+   */
+   public static String toString( Die d ) {
+      return "";
+   }
+
+  /**
+   * A little test main to check things out
+   */
+   public static void main( String[] args ) {
+      Die d = new Die(1);
+      Die q = new Die(2);
+      Die r = new Die(5);
+      Die e = new Die(6);
+      System.out.println( "Hello world from the Die class..." );
+   }
+
+}
