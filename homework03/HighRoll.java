f42ac8f6b4eca0564b8b3056410f1a25e41d233e
elopez47 <35274682+elopez47@users.noreply.github.com>
2018-02-19 18:46:39 -0800

    Update HighRoll.java

--- homework03/HighRoll.java
+++ homework03/HighRoll.java
@@ -10,78 +10,74 @@ import java.io.IOException;
 
 public class HighRoll{
 
-   public static void main( String args[] ) {
+    public static void main( String args[] ) {
       // This line uses the two classes to assemble an "input stream" for the user to type
       // text into the program
-      BufferedReader input = new BufferedReader( new InputStreamReader( System.in ) );
 
-      System.out.println( "\n Welcome to High Roll!! \n" );
-      System.out.println( "Press the 'q' key to quit the program." );
-      System.out.println( "Press the enter key to continue." );
-
-        if (args.length != 2) {
-            throw new IllegalArgumentException();
-        }
-
-        try {
-            // Parse the arguments.
-            int numOfDie = Integer.parseInt(args[0]);
-            int numOfSides = Integer.parseInt(args[1]);
-            DiceSet ds = new DiceSet( numOfDie, numOfSides );
-        }
-
+      if (args.length != 2) {
+        throw new IllegalArgumentException();
+      }
+      // Parse the arguments.
+      int numOfDie = Integer.parseInt(args[0]);
+      int numOfSides = Integer.parseInt(args[1]);
+      try{
 
         while( true ) { //infinite loop, will break when the user hits the 'q' key
-            System.out.print( ">>" ); //prompt
-            String inputLine = null;
+          System.out.print( ">>" );
 
+          System.out.println( "\n\t Welcome to High Roll! \n" );
+          System.out.println( " Press the 'q' key to quit the program." );
 
-        try {
-            inputLine = input.readLine(); // readLine() method reads the line once the user clicks enter
-            if (inputLine.length() == 0 ) { //checks if user entered something
-                System.out.println("enter some text!:");
-            }
+          DiceSet game = new DiceSet( numOfDie, numOfSides );
 
+          System.out.println( "\n\t\t MENU" );
+          System.out.println( "\n Enter '1' to roll all the dice." );
+          System.out.println( "\n Enter '2' to roll a single die." );
+          System.out.println( "\n Enter '3' to calculate the score for this set of rolls." );
+          System.out.println( "\n Enter '4' to save as high score." );
+          System.out.println( "\n Enter '5' to display your high score." );
+          System.out.println( "\n Enter 'q' to end the game." );
+          System.out.print( ">>" ); //prompt
 
-            /*
-            System.out.println( "\n Enter the number of die you wish play with:" );
-            int i = (int)Integer.parseInt(input.readLine());
-            System.out.println( "\n Enter the number of sides you want your dice to have:" );
-            int j = (int)Integer.parseInt(input.readLine());
-            DiceSet game = new DiceSet( i, j );
-            */
+          int highScore = 0;
+          //int k = 0;
+          String inputLine = null;
+          BufferedReader input = new BufferedReader( new InputStreamReader( System.in ) );
+          String option = inputLine;
+          option = input.readLine();
 
-            /*System.out.println( "\t MENU" );
-            System.out.println( "\n Enter '1' to roll all the dice." );
-            System.out.println( "\n Enter '2' to roll a single die." );
-            System.out.println( "\n Enter '3' to calculate the score for this set of rolls." );
-            System.out.println( "\n Enter '4' to save as high score." );
-            System.out.println( "\n Enter '5' to display your high score." );
-            System.out.println( "\n Enter 'q' to end the game." );
-            System.out.print( ">>" ); //prompt
-            */
+          switch (option.charAt(0)) {
+              case '1': game.roll();
+              case '2': System.out.println( HighRoll.optionTwo( game ) );
+              case '3': System.out.println( game.sum() );
+              case '4': highScore = game.sum();
+              case '5': System.out.println("Your high score is: " + highScore);
+              case 'q': System.exit(0);
+              default:
+               throw new IllegalArgumentException();
 
-
-            String option = inputLine;
-
-            int highScore = 0;
-            //int k = 0;
-
-
-
-            switch (option.charAt(0)) {
-               case '1': game.roll();
-               case '2': game.rollIndividual(numOfSides-1);
-               case '3': game.sum();
-               case '4': highScore = game.sum();
-               case '5': System.out.println("Your high score is: " + highScore);
-               case 'q': System.exit(0);
-            }
-            System.out.println( "   You typed: " + inputLine ); // outputs users input
-         }
-         catch( IOException ioe ) {
-            System.out.println( "Caught IOException" );
-         }
+          }
+        }
       }
-   }
-}
+      catch( Exception e ) {
+        System.out.println( "Caught IOException" );
+      }
+    }
+
+    public static int optionTwo(DiceSet ds) {
+      System.out.println( "\n Enter the die you wish to roll." );
+      String dienum = null;
+      BufferedReader input2 = new BufferedReader( new InputStreamReader( System.in ) );
+
+      try{
+        dienum = input2.readLine();
+      }
+      catch(Exception e){
+        System.out.println( "Caught IOException" );
+      }
+
+      int die = Integer.parseInt( dienum );
+      return ds.rollIndividual( die-1 );
+    }
+
+  }
